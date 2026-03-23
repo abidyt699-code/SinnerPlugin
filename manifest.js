70b3ff1a460d3e328ac2926287372f3cc5cc1175
abidyt699-code <abidyt699@gmail.com>
2026-03-24 00:56:53 +0600

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,9 @@
+{
+  "name": "GainBooster",
+  "description": "Simple gain from 1-30dB",
+  "authors": [{"name": "You"}],
+  "main": "GainBooster.js",
+  "vendetta": {
+    "icon": "some-discord-asset-id"
+  }
+}
